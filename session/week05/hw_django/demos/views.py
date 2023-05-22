6a8b5333dbe52c7e85309f7ac4c47ed9c595dccc
{wonhee} <wanny416@sookmyung.ac.kr>
2023-05-22 18:15:43 +0900

    [FEAT] NewDjangoProject 생성

---
+++ session/week05/hw_django/demos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def NewDjangoProject(request):
+    return HttpResponse("장고의 세계로 들어왔습니다.")
