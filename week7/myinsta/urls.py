e288e3524ad4d53f6b7bb3c0c1cf4bc00d6c1d8d
{wonhee} <wanny416@sookmyung.ac.kr>
2023-07-11 21:03:04 +0900

    [FEAT] 9주차 세션~ 오늘도 알찼따^^

--- week7/myinsta/urls.py
+++ week7/myinsta/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from posts.views import url_view, url_parameter_view, function_view, index
+from posts.views import class_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("url/", url_view),
+    path("url/<str:username>/", url_parameter_view),
+    path("fbv/", function_view),
+    path("cbv/", class_view.as_view()),
+    #
+    path("", index, name="index"),
+    path("posts/", include("posts.urls", namespace="posts")),
 ]
--- week7/posts/views.py
+++ week7/posts/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import ListView
+from .models import Post
+
 
 # Create your views here.
+def url_view(request):
+    data = {"code": "001", "msg": "OK"}
+    # return HttpResponse("<h1>url_views</h1>")
+    return JsonResponse(data)
+
+
+def url_parameter_view(request, username):
+    print("마이멜로디")  # print: 터미널에 표시되는 내용
+    # print(username)
+    # print(request.GET)
+    print(f"username:{username}")
+    print(f"request.GET:{request.GET}")
+    return HttpResponse(username)  # return: 화면에 표시되는 내용
+
+
+def function_view(request):
+    print(f"request.method: {request.method}")
+    if request.method == "GET":
+        print(f"request.GET: {request.GET}")
+    elif request.method == "POST":
+        print(f"request.POST: {request.POST}")
+    return render(request, "view.html")
+
+
+class class_view(ListView):
+    model = Post
+    template_name = "cbv_view.html"
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def post_list_view(request):
+    return render(request, "posts/post_list.html")
+
+
+def post_create_view(request):
+    return render(request, "posts/post_form.html")
+
+
+def post_detail_view(request, id):
+    return render(request, "posts/post_detail.html")
+
+
+def post_delete_view(request, id):
+    return render(request, "post_confirm_delete.html")
